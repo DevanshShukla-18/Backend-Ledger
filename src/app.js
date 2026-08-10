cb6a200e6d2a6cc0471f2080d03cb9eb7c31fe37
Devansh Shukla <231263@kit.ac.in>
2026-08-11 02:52:17 +0530

    Add health check route to confirm service status.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const cookieParser = require("cookie-parser");
 
 const app=express();
 
+app.use(express.json());
+app.use(cookieParser());
+
 /**
  * - Required Routes
  */
@@ -11,15 +14,13 @@ const authRouter= require("./routes/auth.routes");
 const accountRouter = require("./routes/account.routes");
 const transactionRoutes = require("./routes/transaction.routes");
 
-
-
 /**
  * - Use Routes
  */
-app.use(express.json());
-app.use(cookieParser());
 
-
+app.get("/",(req,res)=>{
+    res.send("Ledger services is up and running")
+})
 app.use("/api/auth",authRouter);
 app.use("/api/accounts",accountRouter);
 app.use('/api/transactions', transactionRoutes);
